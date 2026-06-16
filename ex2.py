02a9c29e08bc5e95364b27e30fa7f525b340c90d
wagthibes12 <wagthibes12@gmail.com>
2026-06-15 22:51:58 -0300

    ex3.py

--- ex2.py
+++ ex2.py
@@ -5,4 +5,4 @@ numero = int(input("INforme o número da tabuada: "))
 indice = 1
 while indice <=10:
     print(f"{indice} x {numero} = {indice*numero}")
-    indice+=1
+    indice+=
---
+++ ex3.py
@@ -0,0 +1,4 @@
+alvo = int(input("Informe um número"))
+
+for item in range(1,alvo+1,1):
+    print(item)
